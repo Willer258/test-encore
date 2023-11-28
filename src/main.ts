990eedf458b7b5d7daf18d329c6511219dbe56bd
Wilfried Houinlindjonnon <wilfried@wiassur.com>
2023-11-28 14:49:42 +0000

    remove bootstap.css

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import store from "./store";
 // import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import BootstrapVue3 from "bootstrap-vue-3";
 
-import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
 import "./assets/css/main.css";
