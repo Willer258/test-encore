c310ef4e07cb432514c3f9d868deb968ed219a1e
Wilfried Houinlindjonnon <wilfried@wiassur.com>
2023-11-28 14:58:22 +0000

    test

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 // import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import BootstrapVue3 from "bootstrap-vue-3";
+// import BootstrapVue3 from "bootstrap-vue-3";
 
-import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+// import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
 import "./assets/css/main.css";
 import "./assets/css/base.css";
@@ -14,7 +14,7 @@ import Popup from "./entity/Popup";
 
 const app = createApp(App);
 
-app.use(BootstrapVue3);
+// app.use(BootstrapVue3);
 // app.use(IconsPlugin)
 
 app.use(store);
